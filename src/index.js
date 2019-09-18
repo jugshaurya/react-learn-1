79b7c7971f341b6ecdb16dea2109a58fb7765fb2
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2019-09-18 19:28:54 +0530

    added counters component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Counter from "./components/counter";
+import Counters from "./components/counters";
 
-ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<Counters />, document.getElementById("root"));
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById("root"));
+// }
 
+// setInterval(tick, 1000);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
